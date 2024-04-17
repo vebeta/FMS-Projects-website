32f442c9ee6e9142b160cd1937a9d5bdb245e171
ATregubovich-2022 <326632andrey@gmail.com>
2024-04-17 22:34:47 +0700

    edit profile

--- forms.py
+++ forms.py
@@ -19,3 +19,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired(message="Это поле обязательно")])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class EditProfileForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired(message="Это поле обязательно"),
+                                            Email(message="Укажите действительный адрес электронной почты")])
+    surname = StringField('Фамилия', validators=[DataRequired(message="Это поле обязательно")])
+    name = StringField('Имя', validators=[DataRequired(message="Это поле обязательно")])
+    password = PasswordField('Пароль')
+    confirm = PasswordField('Повторите пароль', validators=[EqualTo('password', message='Пароли не совпадают')])
+    submit = SubmitField('Сохранить изменения')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, flash
-from forms import LoginForm, RegisterForm
+from flask import Flask, render_template, redirect, flash, request
+from forms import LoginForm, RegisterForm, EditProfileForm
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from data.users import User
 from data.themes import Themes
@@ -42,7 +42,6 @@ def reqister():
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
             errors["email"].append("Почта с таким адресом уже занята!")
-        print(errors)
         if any(errors.values()):
             return render_template("register.html", title="Регистрация", form=form, errors=errors)
         user = User(surname=form.surname.data,
@@ -76,7 +75,7 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if not user:
             errors["email"].append("Пользователя с таким email не существует!")
-        if not user.check_password(form.password.data):
+        elif not user.check_password(form.password.data):
             errors["password"].append("Неверный пароль")
         if any(errors.values()):
             return render_template('login.html', title="Авторизация", form=form, errors=errors)
@@ -85,10 +84,40 @@ def login():
         return redirect("/")
     return render_template('login.html', title='Авторизация', form=form, errors=errors)
 
-@app.route('/profile')
+
+@app.route('/profile/<int:user_id>')
 @login_required
-def profile():
-    return render_template("profile.html", title="Профиль")
+def profile(user_id):
+    user = load_user(user_id)
+    if not user:
+        return redirect('/')
+    return render_template("profile.html", title="Профиль", user=user)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    errors = {
+        "surname": [],
+        "name": []
+    }
+    db_sess = db_session.create_session()
+    form.email.data = current_user.email
+    if form.validate_on_submit():
+        current_user.surname = form.surname.data
+        current_user.name = form.name.data
+        if form.password.data:
+            current_user.set_password(form.password.data)
+        db_sess.add(current_user)
+        db_sess.commit()
+        flash('Изменения сохранены!')
+        return redirect(f"/profile/{current_user.id}")
+    elif request.method == 'GET':
+        form.name.data = current_user.name
+        form.surname.data = current_user.surname
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form, errors=errors)
 
 
 @app.route('/logout')
