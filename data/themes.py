e986a17c91350869dfc988ad16c38f70db00ce8b
ATregubovich-2022 <326632andrey@gmail.com>
2024-04-18 21:38:43 +0700

    Попытка починить GitHub

---
+++ data/themes.py
@@ -0,0 +1,21 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Themes(SqlAlchemyBase):
+    __tablename__ = 'themes'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    theme = sqlalchemy.Column(sqlalchemy.String)
+    description = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime)
+    status = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+
+    author_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    student_id = sqlalchemy.Column(sqlalchemy.Integer)
+    user = orm.relationship('User')
