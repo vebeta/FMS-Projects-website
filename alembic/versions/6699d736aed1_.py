cacbd582b4d13747b9bfed360a44b5360417fe5c
vladk <vladkhegay19@gmail.com>
2024-04-19 19:59:47 +0700

    Перенёс alembic с гита

---
+++ alembic/versions/6699d736aed1_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 6699d736aed1
+Revises: 
+Create Date: 2024-04-19 17:15:30.311265
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6699d736aed1'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
